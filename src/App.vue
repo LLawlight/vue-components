f15881f9fa88f52075a4ee51ca859dca92308013
me@zhaoyuxiang.cn <me@zhaoyuxiang.cn>
2016-09-26 18:12:03 +0800

    save

---
+++ src/App.vue
@@ -0,0 +1,163 @@
+<template>
+  <div id="app">
+    <div class="header">
+      <img class="logo" src="./assets/logo.png">
+      <hello></hello>
+    </div>
+
+    <div id="modal" class="section">
+      <h2>Modal</h2>
+
+      <div class="introduce">
+        <a class="z-btn" @click.prevent="alert.show = true">Alert</a>
+        <modal
+        :show.sync="alert.show"
+        :content="'这是一个alert！'"
+        ></modal>
+      </div>
+
+      <div class="introduce">
+        <a class="z-btn" @click.prevent="confirm.show = true">confirm</a>
+        <modal
+        :show.sync="confirm.show"
+        :type="'confirm'"
+        :content="'这是一个confirm！'"
+        ></modal>
+      </div>
+    </div>
+
+    <div id="aside" class="section">
+      <h2>Aside</h2>
+      <div class="introduce">
+        <a class="z-btn" @click.prevent="asideLeft.show = true">left</a>
+        <aside
+        :show.sync="asideLeft.show"
+        :type="'left'">
+          <h3>我是Aside-Left</h3>
+          <div>组件内部可以自定义HTML结构</div>
+          <p>直接在&lt;aside&gt;标签中写即可</p>
+          <div>内容长度无限制，超过部分可以滚动浏览</div>
+        </aside>
+      </div>
+
+      <div class="introduce">
+        <a class="z-btn" @click.prevent="asideTop.show = true">top</a>
+        <aside
+        :show.sync="asideTop.show"
+        :type="'top'">
+          <h3>我是Aside-Top</h3>
+          <div>组件内部可以自定义HTML结构</div>
+          <p>直接在&lt;aside&gt;标签中写即可</p>
+          <div>内容长度无限制，超过部分可以滚动浏览</div>
+        </aside>
+      </div>
+
+      <div class="introduce">
+        <a class="z-btn" @click.prevent="asideRight.show = true">right</a>
+        <aside
+        :show.sync="asideRight.show"
+        :type="'right'">
+          <h3>我是Aside-Right</h3>
+          <div>组件内部可以自定义HTML结构</div>
+          <p>直接在&lt;aside&gt;标签中写即可</p>
+          <div>内容长度无限制，超过部分可以滚动浏览</div>
+        </aside>
+      </div>
+
+      <div class="introduce">
+        <a class="z-btn" @click.prevent="asideBottom.show = true">bottom</a>
+        <aside
+        :show.sync="asideBottom.show"
+        :type="'bottom'">
+          <h3>我是Aside-Bottom</h3>
+          <div>组件内部可以自定义HTML结构</div>
+          <p>直接在&lt;aside&gt;标签中写即可</p>
+          <div>内容长度无限制，超过部分可以滚动浏览</div>
+        </aside>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello'
+import Modal from './components/Modal'
+import Aside from './components/Aside'
+
+export default {
+  components: {
+    Hello,
+    Modal,
+    Aside
+  },
+  data() {
+    return {
+      alert: {
+        show: false
+      },
+      confirm: {
+        show: false
+      },
+      asideLeft: {
+        show: false
+      },
+      asideTop: {
+        show: false
+      },
+      asideRight: {
+        show: false
+      },
+      asideBottom: {
+        show: false
+      }
+    }
+  }
+}
+</script>
+
+<style>
+h1,h2 {
+  margin: 0;
+  padding: 0;
+}
+
+#app {
+  color: rgb(53, 73, 94);
+  font-family: Source Sans Pro, Helvetica, sans-serif;
+  font-size: 16px;
+}
+
+a {
+  color: rgb(65, 184, 131);
+  text-decoration: none;
+}
+
+.header {
+  text-align: center;
+}
+
+.logo {
+  width: 100px;
+  height: 100px
+}
+
+.section {
+  margin: 20px;
+}
+
+.introduce {
+  margin: 10px 0;
+  display: inline-block;
+}
+
+.z-btn {
+  display: inline-block;
+  padding: 6px 12px;
+  cursor: pointer;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  background-color: #fff;
+}
+
+</style>
---
+++ src/components/Aside.vue
@@ -0,0 +1,153 @@
+<template lang="html">
+  <div class="z-supernatant" v-show="show" transition="z-dimmer">
+    <div class="z-aside" v-show="show" :transition="'z-aside-'+type">
+      <div class="z-aside-title">
+        {{title}}
+      </div>
+      <div class="z-aside-content">
+        <slot></slot>
+      </div>
+      <div class="z-aside-footer">
+        <a class="z-btn-aside" @click="show = false">{{close}}</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  props: {
+    close: {
+      type: String,
+      default: "关闭"
+    },
+    show: {
+      type: Boolean,
+      twoWay: true,
+      default: false
+    },
+    title: {
+      type: String,
+      default: "标题"
+    },
+    type: {
+      type: String,
+      default: "left"
+    }
+  },
+  computed: {},
+  ready() {},
+  attached() {},
+  methods: {},
+  components: {}
+};
+</script>
+
+<style lang="css">
+.z-supernatant {
+  position: fixed;
+  z-index: 9998;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, .5);
+  display: table;
+}
+
+.z-dimmer-transition {
+  transition: all .3s ease;
+}
+
+.z-dimmer-enter, .z-dimmer-leave {
+  opacity: 0;
+}
+
+.z-aside {
+  background-color: white;
+  position: fixed;
+  transition: all .3s ease;
+}
+
+.z-aside-left-transition {
+  height: 100%;
+  width:300px;
+  left: 0;
+}
+
+.z-aside-left-enter, .z-aside-left-leave {
+  left: -300px;
+}
+
+.z-aside-right-transition {
+  height: 100%;
+  width:300px;
+  right: 0;
+}
+
+.z-aside-right-enter, .z-aside-right-leave {
+  right: -300px;
+}
+
+.z-aside-top-transition {
+  width: 100%;
+  height: 300px;
+  top: 0;
+}
+
+.z-aside-top-enter, .z-aside-top-leave {
+  top: -300px;
+}
+
+.z-aside-bottom-transition {
+  width: 100%;
+  height: 300px;
+  bottom: 0;
+}
+
+.z-aside-bottom-enter, .z-aside-bottom-leave {
+  bottom: -300px;
+}
+
+.z-aside-title {
+  height: 44px;
+  line-height: 44px;
+  text-align: center;
+  border-bottom: 1px solid #ccc;
+  position: absolute;
+  top: 0;
+  width: 100%;
+}
+
+.z-aside-content {
+  width: 100%;
+  overflow: auto;
+  position: absolute;
+  top: 44px;
+  bottom: 44px;
+  padding: 10px;
+  font-size: 14px;
+  box-sizing: border-box;
+}
+
+.z-aside-footer {
+  height: 44px;
+  display: table;
+  width: 100%;
+  position: absolute;
+  bottom: 0;
+  border-top: 1px solid #ccc;
+  z-index: 999;
+  background-color: #fff;
+}
+
+.z-btn-aside {
+  display: table-cell;
+  text-align: center;
+  line-height: 44px;
+  cursor: pointer;
+}
+</style>
