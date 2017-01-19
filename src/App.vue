01862e917bce7c86cb76c46c8fec41c2438e03d8
me@zhaoyuxiang.cn <me@zhaoyuxiang.cn>
2017-01-19 11:32:06 +0800

    尝试修改评论样式

--- src/App.vue
+++ src/App.vue
@@ -143,6 +143,10 @@ export default {
     }
   },
 
+  mounted() {
+    this.changyan()
+  },
+
   watch: {
     '$route.query': function() {
       this.startProgress()
@@ -161,6 +165,14 @@ export default {
 
     getTitle(title) {
       this.componentTitle = title
+    },
+
+    changyan() {
+      var appid = 'cysNYmpbA';
+      var conf = 'prod_0ac045c2d3f8c8f9c94075b367169d93';
+      var width = window.innerWidth || document.documentElement.clientWidth;
+      if (width < 960) {
+      window.document.write('<script id="changyan_mobile_js" charset="utf-8" type="text/javascript" src="http://changyan.sohu.com/upload/mobile/wap-js/changyan_mobile.js?client_id=' + appid + '&conf=' + conf + '"><\/script>'); } else { var loadJs=function(d,a){var c=document.getElementsByTagName("head")[0]||document.head||document.documentElement;var b=document.createElement("script");b.setAttribute("type","text/javascript");b.setAttribute("charset","UTF-8");b.setAttribute("src",d);if(typeof a==="function"){if(window.attachEvent){b.onreadystatechange=function(){var e=b.readyState;if(e==="loaded"||e==="complete"){b.onreadystatechange=null;a()}}}else{b.onload=a}}c.appendChild(b)};loadJs("http://changyan.sohu.com/upload/changyan.js",function(){window.changyan.api.config({appid:appid,conf:conf})}); }
     }
   }
 }
@@ -241,11 +253,11 @@ a {
 }
 
 #SOHUCS {
-  width: 92%;
-  margin: 0 auto 50px;
-  padding: 10px;
-  box-sizing: border-box;
-  background-color: #fff;
+  width: 92% !important;
+  margin: 0 auto 50px !important;
+  padding: 10px !important;
+  box-sizing: border-box !important;
+  background-color: #fff !important;
 }
 
 .content {
@@ -379,8 +391,8 @@ a {
   }
 
   #SOHUCS {
-    width: 100%;
-    margin: 0  0 10px 0;
+    width: 100% !important;
+    margin: 0  0 10px 0 !important;
   }
 
   .content {
