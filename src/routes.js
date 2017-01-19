f284a14881e7484db526be71feecba16764f340d
me@zhaoyuxiang.cn <me@zhaoyuxiang.cn>
2017-01-19 17:11:13 +0800

    路由组件全部换成懒加载，去掉map

--- src/routes.js
+++ src/routes.js
@@ -1,23 +1,41 @@
-import Hello from './components/Hello'
+// import Hello from './components/Hello'
+const Hello = resolve => require(['./components/Hello.vue'], resolve)
 
-import installMd from 'src/docs/Install.md'
-import gridMd from 'src/docs/Grid.md'
+// import installMd from 'src/docs/Install.md'
+// import gridMd from 'src/docs/Grid.md'
+const installMd = resolve => require(['src/docs/Install.md'], resolve)
+const gridMd = resolve => require(['src/docs/Grid.md'], resolve)
 
-import actionsheetMd from 'src/docs/ActionSheet.md'
-import asideMd from 'src/docs/Aside.md'
-import backtopMd from 'src/docs/BackTop.md'
-import buttonMd from 'src/docs/Button.md'
-import carrouselMd from 'src/docs/Carrousel.md'
-import dimmerMd from 'src/docs/Dimmer.md'
-import dropmenuMd from 'src/docs/Dropmenu.md'
-import imageinputMd from 'src/docs/ImageInput.md'
-import inputnumberMd from 'src/docs/InputNumber.md'
-import modalMd from 'src/docs/Modal.md'
-import navbarMd from 'src/docs/Navbar.md'
-import paginationMd from 'src/docs/Pagination.md'
-import progressMd from 'src/docs/Progress.md'
-import switchMd from 'src/docs/Switch.md'
-import tableMd from 'src/docs/Table.md'
+// import actionsheetMd from 'src/docs/ActionSheet.md'
+// import asideMd from 'src/docs/Aside.md'
+// import backtopMd from 'src/docs/BackTop.md'
+// import buttonMd from 'src/docs/Button.md'
+// import carrouselMd from 'src/docs/Carrousel.md'
+// import dimmerMd from 'src/docs/Dimmer.md'
+// import dropmenuMd from 'src/docs/Dropmenu.md'
+// import imageinputMd from 'src/docs/ImageInput.md'
+// import inputnumberMd from 'src/docs/InputNumber.md'
+// import modalMd from 'src/docs/Modal.md'
+// import navbarMd from 'src/docs/Navbar.md'
+// import paginationMd from 'src/docs/Pagination.md'
+// import progressMd from 'src/docs/Progress.md'
+// import switchMd from 'src/docs/Switch.md'
+// import tableMd from 'src/docs/Table.md'
+const actionsheetMd = resolve => require(['src/docs/ActionSheet.md'], resolve)
+const asideMd = resolve => require(['src/docs/Aside.md'], resolve)
+const backtopMd = resolve => require(['src/docs/BackTop.md'], resolve)
+const buttonMd = resolve => require(['src/docs/Button.md'], resolve)
+const carrouselMd = resolve => require(['src/docs/Carrousel.md'], resolve)
+const dimmerMd = resolve => require(['src/docs/Dimmer.md'], resolve)
+const dropmenuMd = resolve => require(['src/docs/Dropmenu.md'], resolve)
+const imageinputMd = resolve => require(['src/docs/ImageInput.md'], resolve)
+const inputnumberMd = resolve => require(['src/docs/InputNumber.md'], resolve)
+const modalMd = resolve => require(['src/docs/Modal.md'], resolve)
+const navbarMd = resolve => require(['src/docs/Navbar.md'], resolve)
+const paginationMd = resolve => require(['src/docs/Pagination.md'], resolve)
+const progressMd = resolve => require(['src/docs/Progress.md'], resolve)
+const switchMd = resolve => require(['src/docs/Switch.md'], resolve)
+const tableMd = resolve => require(['src/docs/Table.md'], resolve)
 
 const routes = [
   { path: '/', component: Hello},
